4fd138d6bf15f3879ae01ada437367fb3df6d76c
Yusef-H <yusefhalabi456@gmail.com>
2022-10-08 22:23:42 +0300

    add winner player check

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ const displayController = (function(){
     const renderBoard = () => {
         for(let i = 0; i < 9; i++){
             const cellElement = _createBoardCell();
+            cellElement.value = i;
             gameBoardElement.appendChild(cellElement);
         }
     };
@@ -16,6 +17,7 @@ const displayController = (function(){
         const gameBoard = Array.from(gameBoardElement.childNodes);
         const chosenCell = gameBoard.find((cell) => spotChoice == cell);
         if(chosenCell.textContent == ""){
+            player.patterns.add(chosenCell.value);
             chosenCell.textContent = player.letter;
         }
     }
@@ -37,9 +39,10 @@ const displayController = (function(){
 
 // letter is "X" or "O"
 const PlayerFactory = function(letter){
-    
+    let patterns = new Set();
     return {
-        letter
+        letter,
+        patterns
     }
 };
 
@@ -51,23 +54,36 @@ const gameModule = (function(){
     displayController.renderBoard();
     let turn = 1;
 
-    const _initializeListeners = () =>{
-        const boardElement = displayController.getGameBoardElement();
-        const boardArray = Array.from(boardElement.childNodes);
-        boardArray.forEach((cell) => {
-            cell.addEventListener('click', () => {
-                displayController.markSpot((turn == 1 ? firstPlayer:secondPlayer), cell);
-                turn = 3 - turn;
-            })
-        })
+    const checkWin = (player) =>{
+        const choiceSet = player.patterns;
+        if((player.patterns.has(0) && player.patterns.has(1) && player.patterns.has(2)) ||
+        (player.patterns.has(3) && player.patterns.has(4) && player.patterns.has(5)) ||
+        (player.patterns.has(6) && player.patterns.has(7) && player.patterns.has(8)) || 
+        (player.patterns.has(0) && player.patterns.has(3) && player.patterns.has(6)) ||
+        (player.patterns.has(1) && player.patterns.has(4) && player.patterns.has(7)) ||
+        (player.patterns.has(2) && player.patterns.has(5) && player.patterns.has(8)) ||
+        (player.patterns.has(0) && player.patterns.has(4) && player.patterns.has(8)) ||
+        (player.patterns.has(2) && player.patterns.has(4) && player.patterns.has(6)) ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
+
+    const boardElement = displayController.getGameBoardElement();
+    const boardArray = Array.from(boardElement.childNodes);
+    boardArray.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            displayController.markSpot((turn == 1 ? firstPlayer:secondPlayer), cell);
+            turn = 3 - turn;
+            if(checkWin(firstPlayer)){
+                console.log("first won");
+            }
+            if(checkWin(secondPlayer)){
+                console.log("Second won");
+            }
+        })
+    })
     
-
-    _initializeListeners();
-
-
-
-    
-
-
 })();
