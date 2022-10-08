b36d7942a220539238e0fda00971912c404804b3
Yusef-H <yusefhalabi456@gmail.com>
2022-10-08 21:52:26 +0300

    add mark spot functionality

--- game.js
+++ game.js
@@ -2,14 +2,23 @@
 const displayController = (function(){
     let gameBoardElement = document.querySelector('.game-board');
 
+    const getGameBoardElement = () =>{
+        return gameBoardElement;
+    }
     const renderBoard = () => {
         for(let i = 0; i < 9; i++){
             const cellElement = _createBoardCell();
             gameBoardElement.appendChild(cellElement);
-            console.log("hi");
-
         }
     };
+
+    const markSpot = (player, spotChoice) =>{
+        const gameBoard = Array.from(gameBoardElement.childNodes);
+        const chosenCell = gameBoard.find((cell) => spotChoice == cell);
+        if(chosenCell.textContent == ""){
+            chosenCell.textContent = player.letter;
+        }
+    }
 
     const _createBoardCell = () => {
         const cell = document.createElement('div');
@@ -17,27 +26,48 @@ const displayController = (function(){
         return cell;
     }
 
+   
+
     return {
-        gameBoardElement,
-        renderBoard
+        getGameBoardElement,
+        renderBoard,
+        markSpot
     }
 })();
 
-
-const gameBoardModule = (function(){
-    // Will make render board function from display controller
-    displayController.renderBoard();
-    gameBoard = [];
-
-})()
+// letter is "X" or "O"
+const PlayerFactory = function(letter){
+    
+    return {
+        letter
+    }
+};
 
 const gameModule = (function(){
     // initialize players
+    const firstPlayer = PlayerFactory("X");
+    const secondPlayer = PlayerFactory("O");
     // initialize gameboard
+    displayController.renderBoard();
+    let turn = 1;
+
+    const _initializeListeners = () =>{
+        const boardElement = displayController.getGameBoardElement();
+        const boardArray = Array.from(boardElement.childNodes);
+        boardArray.forEach((cell) => {
+            cell.addEventListener('click', () => {
+                displayController.markSpot((turn == 1 ? firstPlayer:secondPlayer), cell);
+                turn = 3 - turn;
+            })
+        })
+    }
+    
+
+    _initializeListeners();
+
+
+
+    
 
 
 })();
-
-const playerFactory = function(){
-
-};
